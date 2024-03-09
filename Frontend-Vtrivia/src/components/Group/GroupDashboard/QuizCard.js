d7ffc7cf6e40ffdbfc1d250c4356fa385c329155
Aryan Sanghi <aryansanghi008@gmail.com>
2024-03-09 20:20:41 +0530

    sixth commit

--- Frontend-Vtrivia/src/components/Group/GroupDashboard/QuizCard.js
+++ Frontend-Vtrivia/src/components/Group/GroupDashboard/QuizCard.js
@@ -1,10 +1,23 @@
 import React from "react";
 import groupIcon from "../../../assets/QuizLogo.png";
- 
-const QuizCard = ({name}) => {
+import AttemptQuizModal from "./AttemptQuizModal";
+
+const QuizCard = ({ name }) => {
+  const [attemptQuiz, setAttemptQuiz] = useState(false);
+
+  const openAttemptQuizModal = () => {
+    setAttemptQuiz(true);
+  };
+
+  const closeAttemptQuizModal = () => {
+    setAttemptQuiz(false);
+  };
   console.log(name);
   return (
-    <div className="mt-5 ml-3 max-w-60 max-h-96 rounded-3xl overflow-hidden shadow-md">
+    <div
+      className="mt-5 ml-3 max-w-60 max-h-96 rounded-3xl overflow-hidden shadow-md"
+      onClick={openAttemptQuizModal}
+    >
       <img
         src={groupIcon}
         alt="group-icon"
@@ -14,8 +27,13 @@ const QuizCard = ({name}) => {
         <div className="font-bold text-xl mb-2">{name}</div>
         <p className="text-gray-700 text-base">{name}</p>
       </div>
+      <AttemptQuizModal
+        grpId={props.curr_group.id}
+        isOpen={attemptQuiz}
+        onClose={closeAttemptQuizModal}
+      />
     </div>
   );
 };
- 
+
 export default QuizCard;
