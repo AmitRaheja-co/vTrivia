434e4537152e287a9714639d5112d079488eeaad
Amit <amitraheja291@gmail.com>
2024-03-19 21:59:18 +0530

    presentation code commit

--- Frontend-Vtrivia/src/components/HomePage/FriendCard.js
+++ Frontend-Vtrivia/src/components/HomePage/FriendCard.js
@@ -7,7 +7,7 @@ const FriendCard = (props) => {
         <div className="flex flex-col items-center pb-10">
           <img
             className="w-24 h-24 mb-3 rounded-full shadow-lg"
-            src={props.image}
+            src={props.photo}
             alt="friend-image"
           />
           <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
